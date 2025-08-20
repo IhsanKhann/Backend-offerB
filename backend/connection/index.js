e8553fddbb92962d69c8491ba65fb6192261d182
IhsanKhann <mortalihsan@gmail.com>
2025-08-20 16:53:14 +0500

    connection fix

--- backend/connection/index.js
+++ backend/connection/index.js
@@ -6,14 +6,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
+    if (!process.env.MONGODB_URI) {
       console.log("❌ MONGO_URI environment variable is not set");
       return;
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
     });
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
